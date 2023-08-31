4b94daa5b0aa1ddcdc1b83e1ccf0f25bf101cba5
cgkim93 <cgkim93@lsnikko.com>
2023-08-31 14:54:58 +0900

    Initial commit

---
+++ src/main/java/com/opc/mreport/OpcMReportApplication.java
@@ -0,0 +1,13 @@
+package com.opc.mreport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpcMReportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpcMReportApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/opc/mreport/OpcMReportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.opc.mreport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpcMReportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
